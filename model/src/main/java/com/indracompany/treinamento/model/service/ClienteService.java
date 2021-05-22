75797ef27ab702d0f2ce5ec35123974ca8ce0978
Felq <felqlima@gmail.com>
2021-05-22 18:16:22 -0300

    teste

---
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -0,0 +1,50 @@
+package com.indracompany.treinamento.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.indracompany.treinamento.exception.AplicacaoException;
+import com.indracompany.treinamento.exception.ExceptionValidacoes;
+import com.indracompany.treinamento.model.dto.ClienteDTO;
+import com.indracompany.treinamento.model.entity.Cliente;
+import com.indracompany.treinamento.model.repository.ClienteRepository;
+import com.indracompany.treinamento.util.CpfUtil;
+
+@Service
+public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRepository>{
+
+	  public ClienteDTO buscarClientePorCpf(String cpf) {
+		  
+		  if (!cpfEhValido(cpf)) {
+			  throw new AplicacaoException(ExceptionValidacoes.ERRO_CPF_INVALIDO);
+		  }
+		  
+		  Cliente cli = getRepository().findByCpf(cpf);
+		  
+		  ClienteDTO retorno = new ClienteDTO();
+		  retorno.setEmail(cli.getEmail());
+		  retorno.setNome(cli.getNome());
+		  retorno.setCpf(cli.getCpf());
+		  retorno.setId(cli.getId());
+		  
+		  return retorno;
+	  }
+	  
+	  private boolean cpfEhValido(String cpf) {
+		  return CpfUtil.validaCPF(cpf);
+	  }
+	  
+	  public ClienteDTO buscarClientePorNome(String nome) {
+		  
+		  Cliente cli = getRepository().findByNome(nome);
+		  
+		  ClienteDTO retorno = new ClienteDTO();
+		  retorno.setEmail(cli.getEmail());
+		  retorno.setNome(cli.getNome());
+		  retorno.setCpf(cli.getCpf());
+		  retorno.setId(cli.getId());
+		  
+		  return retorno;
+	  }
+=======
+>>>>>>> master
+}
